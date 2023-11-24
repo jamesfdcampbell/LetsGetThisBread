fa7c47d567479f02a65fda454be02f8f973eb04d
James Campbell <james@jamescampell.ca>
2023-11-24 12:20:35 -0600

    fixed issues with activeadmin

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    def self.ransackable_attributes(auth_object = nil)
+        ["category", "created_at", "description", "id", "id_value", "name", "price", "updated_at"]
+    end
 end
