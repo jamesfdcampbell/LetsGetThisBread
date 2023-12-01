875dcad0a6334f68f1fe33305d3861bb617bd0ac
James Campbell <james@jamescampell.ca>
2023-12-01 11:34:21 -0600

    created initial about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/about'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :products
@@ -11,7 +12,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get '/admin', to: "admin#dashboard"
+  get '/admin', to: 'admin#dashboard'
+
+  get '/about', to: 'pages#about'
 
   # Defines the root path route ("/")
   root to: "products#index"
